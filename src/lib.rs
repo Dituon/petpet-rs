8e613a258a661592106c65db12d33f3284fde7e1
Dituon <68615161+Dituon@users.noreply.github.com>
2023-12-08 21:57:28 +0900

    feat(Java Binding): init

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,29 @@
 extern crate alloc;
+#[cfg(feature = "java")]
+extern crate jni;
 
-use std::sync::Arc;
-use pyo3::exceptions::PyValueError;
+#[cfg(feature = "java")]
+use jni::JNIEnv;
+#[cfg(feature = "java")]
+use jni::objects::{JByteArray, JClass, JString};
+#[cfg(feature = "java")]
+use once_cell::sync::Lazy;
 #[cfg(feature = "python")]
 use pyo3::prelude::*;
+#[cfg(feature = "python")]
 use pyo3::prelude::Python;
+#[cfg(feature = "python")]
 use pyo3::types::{PyBytes, PyString, PyTuple};
 #[cfg(feature = "python")]
 use pythonize::depythonize;
+#[cfg(feature = "python")]
+use pyo3::exceptions::PyValueError;
+#[cfg(feature = "python")]
+use std::sync::Arc;
 
 use crate::core::builder::petpet_builder::PetpetBuilder;
 use crate::core::encoder::encoder::{EncodeFormat, IMAGE_ENCODER};
+use crate::core::errors::Error;
 use crate::core::http::avatar_data_factory::create_avatar_data;
 use crate::core::http::template_data::PetpetData;
 use crate::core::template::petpet_template::PetpetTemplate;
@@ -26,9 +39,11 @@ struct PyPetpetBuilder {
 #[cfg(feature = "python")]
 #[pyclass]
 enum PyOutputFormat {
-    GIF, PNG
+    GIF,
+    PNG,
 }
 
+#[cfg(feature = "python")]
 impl From<EncodeFormat> for PyOutputFormat {
     fn from(value: EncodeFormat) -> Self {
         match value {
@@ -38,6 +53,7 @@ impl From<EncodeFormat> for PyOutputFormat {
     }
 }
 
+#[cfg(feature = "python")]
 #[pymethods]
 impl PyPetpetBuilder {
     #[new]
@@ -65,7 +81,7 @@ impl PyPetpetBuilder {
             let avatar_data = create_avatar_data(&data.avatar).unwrap();
             let (images, delay) = builder.build(
                 avatar_data,
-                data.text
+                data.text,
             ).await.unwrap();
             let (blob, format) = IMAGE_ENCODER.encode(&images, delay).unwrap();
 
@@ -74,7 +90,7 @@ impl PyPetpetBuilder {
                     py, vec![
                         PyBytes::new(py, &blob).into_py(py),
                         PyOutputFormat::from(format).into_py(py),
-                    ]
+                    ],
                 ).into_py(py)
             );
             Ok(bytes)
@@ -89,3 +105,84 @@ fn petpet(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyOutputFormat>()?;
     Ok(())
 }
+
+
+#[cfg(feature = "java")]
+static RUNTIME: Lazy<tokio::runtime::Runtime> = Lazy::new(||
+    tokio::runtime::Runtime::new().unwrap()
+);
+
+#[cfg(feature = "java")]
+#[no_mangle]
+pub extern "C" fn Java_moe_d2n_petpetrs_PetpetRsBuilder_createBuilder<'local>(
+    mut env: JNIEnv<'local>, _class: JClass<'local>,
+    template: JString<'local>, path: JString<'local>,
+) -> *const PetpetBuilder {
+    let template_raw: String = env.get_string(&template).expect("Couldn't get java string!").into();
+    let path: String = env.get_string(&path).expect("Couldn't get java string!").into();
+
+    match create_builder(template_raw, path) {
+        Ok(builder) => {
+            Box::into_raw(Box::new(builder))
+        }
+        Err(e) => {
+            let _ = env.throw_new("java/lang/RuntimeException", format!("{:?}", e));
+            std::ptr::null()
+        }
+    }
+}
+
+#[cfg(feature = "java")]
+fn create_builder(template: String, path: String) -> Result<PetpetBuilder, Error>{
+    let template: PetpetTemplate = serde_json::from_str(&template)?;
+    PetpetBuilder::new(template, path)
+}
+
+#[cfg(feature = "java")]
+#[no_mangle]
+pub unsafe extern "C" fn Java_moe_d2n_petpetrs_PetpetRsBuilder_builderBuild<'local>(
+    mut env: JNIEnv<'local>, class: JClass<'local>,
+    ptr: *mut PetpetBuilder,
+    data: JString<'local>,
+) -> JByteArray<'local> {
+    if ptr.is_null() {
+        let _ = env.throw_new("java/lang/NullPointerException", "");
+        return JByteArray::default();
+    }
+
+    match builder_build(&mut env, class, ptr, data) {
+        Ok(arr) => arr,
+        Err(e) => {
+            let _ = env.throw_new("java/lang/RuntimeException", format!("{:?}", e));
+            JByteArray::default()
+        }
+    }
+}
+
+#[cfg(feature = "java")]
+unsafe fn builder_build<'local>(
+    env: &mut JNIEnv<'local>, _class: JClass<'local>,
+    ptr: *mut PetpetBuilder,
+    data: JString<'local>,
+) -> Result<JByteArray<'local>, Error> {
+    let data_raw: String = env.get_string(&data).expect("Couldn't get java string!").into();
+    let data: PetpetData = serde_json::from_str(&data_raw)?;
+    let builder = Box::from_raw(ptr);
+    let avatar_data = create_avatar_data(&data.avatar)?;
+    let (images, delay) = RUNTIME.block_on(
+        builder.build(avatar_data, data.text)
+    )?;
+    let (blob, _format) = IMAGE_ENCODER.encode(&images, delay)?;
+    Ok(env.byte_array_from_slice(&blob).unwrap())
+}
+
+#[cfg(feature = "java")]
+#[no_mangle]
+pub unsafe extern "C" fn Java_moe_d2n_petpetrs_PetpetRsBuilder_closeBuilder(_env: JNIEnv, _class: JClass, ptr: *mut PetpetBuilder) {
+    if ptr.is_null() {
+        return;
+    }
+    let builder = Box::from_raw(ptr);
+    std::mem::drop(builder)
+}
+
